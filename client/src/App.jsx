99577354b5eadbadd9ff181701054f65fcc3efef
Rohit1029 <rohitkumar1028c@gmail.com>
2024-09-09 00:48:59 +0530

    add image silder to listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import About from './pages/About.jsx';
 import Header from "./components/Header.jsx";
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
 import PrivateRoute from "./components/PrivateRoute.jsx";
 
 
@@ -24,6 +25,7 @@ export default function App()
             <Route path="/sign-in" element={<SignIn/>} />
             <Route path="/sign-up" element={<SignUp/>} />
             <Route path="/about" element={<About/>} />
+            <Route path='/listing/:listingId' element={<Listing />} />
             <Route element={<PrivateRoute/>}>
             <Route path="/profile" element={<Profile/>} />
             <Route path='/create-listing' element={<CreateListing />} />
